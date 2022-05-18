9171b3d6ede9ee37155976d91fa2e989d7206934
David Gray <davidgray@Davids-MacBook-Pro.local>
2022-05-18 11:07:23 -0500

    Moved Calls Around
    Also fixed the stat function, it should now call the files/stat instead of object stat endpoint.
    Moved the tolkien cat calls to *before* the stat calls, so it won't fail on that anymore.

--- core/src/link_resolver.rs
+++ core/src/link_resolver.rs
@@ -173,6 +173,17 @@ impl LinkResolverTrait for LinkResolver {
         trace!(logger, "IPFS cache miss"; "hash" => &path);
 
         let (size, client) = select_fastest_client_with_stat(
+        let dat = self.clients.get(0);
+        if let Some(cli) = dat {
+            let byt = cli.tolkien_cat(&path.clone(),Duration::from_secs(3)).await;
+            if let Ok(x) = byt {
+                return Ok(Vec::<u8>::from(x.as_ref()));
+            }
+        }
+
+
+
+        let (stat, client) = select_fastest_client_with_stat(
             self.clients.cheap_clone(),
             logger.cheap_clone(),
             StatApi::Files,
